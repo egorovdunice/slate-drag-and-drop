89d6e658a88e1c25b5fd066acb67c1d81c12ead4
Alexander Egorov <a.egorov@dunice.net>
2020-02-17 11:16:39 +0300

    Added hover effect for drag handler

--- src/slate-drag-and-drop/index.js
+++ src/slate-drag-and-drop/index.js
@@ -10,9 +10,17 @@ export default function DragAndDrop({ref, setReadOnly}) {
     }
   }, [ref]);
 
+  const onStart = () => {
+    document.querySelectorAll('.slate-sortable-handle').forEach((e) => e.classList.remove('slate-sortable-handle-hover'));
+  };
+
+  const onEnd = () => {
+    document.querySelectorAll('.slate-sortable-handle').forEach((e) => e.classList.add('slate-sortable-handle-hover'));
+  };
+
   const createHandler = () => {
     let handler = document.createElement('div');
-    handler.className = "slate-sortable-handle";
+    handler.className = "slate-sortable-handle slate-sortable-handle-hover";
     handler.onmouseenter = () => setReadOnly(true);
     handler.onmouseleave = () => setReadOnly(false);
     return handler
@@ -22,6 +30,8 @@ export default function DragAndDrop({ref, setReadOnly}) {
     if (ref.current) Sortable.create(
       document.getElementById(ref.current.props.id),
       {
+        onEnd,
+        onStart,
         onUpdate,
         ghostClass: 'slate-sortable-ghost',
         dragClass: 'slate-sortable-drag',
